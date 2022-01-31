f764bcbbfbbc349d15370103ec0404cee005bcc4
rafaelurben <github@rafaelurben.ch>
2022-01-31 18:18:20 +0100

    Removed account pages and more

---
+++ orgatask/api_utils.py
@@ -0,0 +1,2 @@
+# Utils for apis
+
--- orgatask/views.py
+++ orgatask/views.py
@@ -12,45 +12,27 @@ def home(request):
     "Show the home page"
     return render(request, 'orgatask/home.html')
 
-
-# Account views
-
-@login_required(login_url=reverse_lazy('orgatask:account-login'))
-def account_logout_done(request):
-    "Show a message and redirect to the login page."
-    messages.success(request, _("Logout erfolgreich!"))
-    return redirect(reverse('orgatask:account-login'))
-
-
-def account_register(request):
-    "Show the registration form"
-    # TODO: implement the registration form
-    return render(request, 'orgatask/account/register.html')
-
-
 # App views
 
 @login_required(login_url=reverse_lazy('orgatask:account-login'))
-def app_main(request):
-    "Show the main page"
-    return render(request, 'orgatask/app/main.html')
+def app(request):
+    "Show the app page"
+    return render(request, 'orgatask/app.html')
 
-@login_required(login_url=reverse_lazy('orgatask:account-login'))
-def app_settings(request):
-    "Show the settings page"
-    return render(request, 'orgatask/app/settings.html')
+def manifest(request):
+    "Render the manifest.json file"
+    response = render(request, "orgatask/manifest.json", {})
+    response['Content-Type'] = 'text/json'
+    response["Service-Worker-Allowed"] = reverse('orgatask:home')
+    return response
 
-@login_required(login_url=reverse_lazy('orgatask:account-login'))
-def app_api(request):
+# API views
+
+# TODO: write API decorators
+@login_required(login_url=reverse_lazy('account:login')+'?next=')
+def api(request):
     "Process API calls"
     # TODO: implement the API
-
-def app_manifest(request):
-    "Render the manifest.json file"
-    response = render(request, "orgatask/app/manifest.json", {})
-    response['Content-Type'] = 'text/json'
-    response["Service-Worker-Allowed"] = reverse('orgatask:app-main')
-    return response
 
 
 # Error views
