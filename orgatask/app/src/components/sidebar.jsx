82e7fcaa18fde8ac620be48f9c413a1dc6d967d7
rafaelurben <github@rafaelurben.ch>
2022-05-24 13:56:39 +0200

    Fixed sidebar overflow

--- orgatask/app/src/components/sidebar.jsx
+++ orgatask/app/src/components/sidebar.jsx
@@ -30,7 +30,7 @@ export default class AppSidebar extends React.Component {
     
     return (
       <div className="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark sidebar h-100">
-        <ul className="nav nav-pills flex-column mb-auto">
+        <ul className="nav nav-pills flex-column mb-auto flex-nowrap overflow-scroll">
           <li>
             <a
               href="#"
