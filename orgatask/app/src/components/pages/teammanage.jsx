5442d0a66ffcdf16c6eb415e3d792eacd1b17043
rafaelurben <github@rafaelurben.ch>
2022-06-14 16:12:59 +0200

    Fixed create invite button colspan

--- orgatask/app/src/components/pages/teammanage.jsx
+++ orgatask/app/src/components/pages/teammanage.jsx
@@ -341,7 +341,7 @@ export default class Page_TeamManage extends React.Component {
                 <tbody>
                   {inviterows}
                   <tr>
-                    <td>
+                    <td colspan="6">
                       <button
                         type="button"
                         className="btn btn-outline-primary border-1"
