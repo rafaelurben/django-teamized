ae8cc1d900f8b78d8191f935298f1503c7dea10b
rafaelurben <github@rafaelurben.ch>
2022-09-10 17:14:50 +0200

    Updated in-app homepage

--- orgatask/app/src/components/pages/home.jsx
+++ orgatask/app/src/components/pages/home.jsx
@@ -11,14 +11,40 @@ export default class Page_Home extends React.Component {
   render() {
     return (
       <Dashboard.Dashboard
-        title={`Hallo ${this.props.user.first_name}`}
-        subtitle="Verwende die Menu- und Seitenleiste, um zu navigieren."
+        title="Startseite"
+        subtitle={`Hallo ${this.props.user.first_name}!`}
       >
-        <Dashboard.DashboardColumn>
-          
+        <Dashboard.DashboardColumn sizes={{ lg: 8 }}>
+          <Dashboard.DashboardTile title="Willkommen">
+            <p className="ms-1">
+              Verwende die Menuleiste (links) und die Seitenleiste (rechts), um zu navigieren.
+            </p>
+          </Dashboard.DashboardTile>
         </Dashboard.DashboardColumn>
+        <Dashboard.DashboardColumn sizes={{ lg: 4 }}>
+          <Dashboard.DashboardTile title="Neuste Features">
+            <div className="ms-1 mt-1">
+              <h6>
+                <b>10. September 2022</b>
+              </h6>
+              <ul className="small">
+                <li>Startseite &amp; In-App-Startseite hinzugefügt</li>
+              </ul>
+              <h6>
+                <b>5. September 2022</b>
+              </h6>
+              <ul className="small">
+                <li>Besseres Abonnier-Erlebnis bei Kalendern</li>
+                <li>Kalender-Features sind nun aus der Beta</li>
+              </ul>
+            </div>
+          </Dashboard.DashboardTile>
+        </Dashboard.DashboardColumn>
+
         <div className="w-50 position-fixed bottom-0 end-0 text-end">
-          <a onClick={Utils.toggleDebug} className="btn text-white">Toggle DEBUG mode</a>
+          <a onClick={Utils.toggleDebug} className="btn text-white">
+            Toggle DEBUG mode
+          </a>
         </div>
       </Dashboard.Dashboard>
     );
