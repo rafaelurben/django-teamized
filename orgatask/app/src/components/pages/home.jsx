e1551609874cf7f966b095d78e15f06a4684bbe3
rafaelurben <github@rafaelurben.ch>
2022-10-11 16:00:06 +0200

    Updated changelog

--- orgatask/app/src/components/pages/home.jsx
+++ orgatask/app/src/components/pages/home.jsx
@@ -57,6 +57,12 @@ export default class Page_Home extends React.Component {
           <Dashboard.Tile title="Neuste Updates" help="Neue Funktionen, Bugfixes und Änderungen">
             <div className="ms-1 mt-1">
               <h6>
+                <b>11. Oktober 2022</b>
+              </h6>
+              <ul className="small">
+                <li>Diverse Fehlerbehebungen</li>
+              </ul>
+              <h6>
                 <b>10. Oktober 2022</b>
               </h6>
               <ul className="small">
