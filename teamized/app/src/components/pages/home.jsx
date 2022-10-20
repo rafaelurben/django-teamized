2b2f3c07f2d71549929bbfd3a097c39a6467d17c
rafaelurben <github@rafaelurben.ch>
2022-10-20 16:26:03 +0200

    Updated changelog

--- teamized/app/src/components/pages/home.jsx
+++ teamized/app/src/components/pages/home.jsx
@@ -57,6 +57,12 @@ export default class Page_Home extends React.Component {
           <Dashboard.Tile title="Neuste Updates" help="Neue Funktionen, Bugfixes und Änderungen">
             <div className="ms-1 mt-1">
               <h6>
+                <b>20. Oktober 2022</b>
+              </h6>
+              <ul className="small">
+                <li>Kalender und To-do-Listen aktualisiert</li>
+              </ul>
+              <h6>
                 <b>18. Oktober 2022</b>
               </h6>
               <ul className="small">
