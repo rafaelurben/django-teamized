9e9bf4a79faa5b43163a9187cb0cb47d494d4edd
rafaelurben <github@rafaelurben.ch>
2023-07-20 01:23:22 +0200

    Updated changelog

--- teamized/app/src/components/pages/home.jsx
+++ teamized/app/src/components/pages/home.jsx
@@ -4,6 +4,8 @@
  * Home page component
  */
 
+import { CHANGELOG } from "../../data/changelog.js";
+import { getDateString } from "../../utils/datetime.js";
 import * as Dashboard from "../dashboard.js";
 import * as Utils from "../../utils/utils.js";
 import * as Settings from "../../utils/settings.js";
@@ -12,6 +14,7 @@ import * as Navigation from "../../utils/navigation.js";
 export default class Page_Home extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { expandChangelog: false };
   }
 
   applyAppearance(evt) {
@@ -21,6 +24,8 @@ export default class Page_Home extends React.Component {
   }
 
   render() {
+    let changelogitems = this.state.expandChangelog ? CHANGELOG : CHANGELOG.slice(0, 5);
+
     return (
       <Dashboard.Page
         title="Startseite"
@@ -30,114 +35,119 @@ export default class Page_Home extends React.Component {
         <Dashboard.Column sizes={{ lg: 8 }}>
           <Dashboard.Tile title="Willkommen">
             <p className="ms-1 mb-1">
-              <span>Verwende die Menuleiste (rechts) und die Seitenleiste (links), um zu navigieren.</span>
+              <span>
+                Verwende die Menuleiste (rechts) und die Seitenleiste (links),
+                um zu navigieren.
+              </span>
               <span className="d-md-none" aria-hidden="true">
                 <br />
-                <b>Hinweis</b>: Diese Seite wurde zwar auch für mobile Geräte optimiert, funktioniert aber besser auf grösseren Geräten.
+                <b>Hinweis</b>: Diese Seite wurde zwar auch für mobile Geräte
+                optimiert, funktioniert aber besser auf grösseren Geräten.
               </span>
-              <a onClick={Utils.toggleDebug} style={{opacity: 0}} className="ms-1" aria-hidden="true">
+              <a
+                onClick={Utils.toggleDebug}
+                style={{ opacity: 0 }}
+                className="ms-1"
+                aria-hidden="true"
+              >
                 DEBUG
               </a>
             </p>
           </Dashboard.Tile>
           <Dashboard.Tile title="Einstellungen">
             <div className="mb-2 ms-1">
-                <label htmlFor="appearance" className="form-label">Erscheinungsbild</label>
-                <div className="btn-group form-control px-2" role="group">
-                  <input type="radio" className="btn-check" name="appearance" value="dark" id="appearance_dark" onChange={this.applyAppearance} checked={this.props.settings.darkmode === true} />
-                  <label className="btn btn-outline-primary" htmlFor="appearance_dark">Dunkel</label>
+              <label htmlFor="appearance" className="form-label">
+                Erscheinungsbild
+              </label>
+              <div className="btn-group form-control px-2" role="group">
+                <input
+                  type="radio"
+                  className="btn-check"
+                  name="appearance"
+                  value="dark"
+                  id="appearance_dark"
+                  onChange={this.applyAppearance}
+                  checked={this.props.settings.darkmode === true}
+                />
+                <label
+                  className="btn btn-outline-primary"
+                  htmlFor="appearance_dark"
+                >
+                  Dunkel
+                </label>
 
-                  <input type="radio" className="btn-check" name="appearance" value="auto" id="appearance_auto" onChange={this.applyAppearance} checked={this.props.settings.darkmode === null}/>
-                  <label className="btn btn-outline-primary" htmlFor="appearance_auto">Automatisch</label>
+                <input
+                  type="radio"
+                  className="btn-check"
+                  name="appearance"
+                  value="auto"
+                  id="appearance_auto"
+                  onChange={this.applyAppearance}
+                  checked={this.props.settings.darkmode === null}
+                />
+                <label
+                  className="btn btn-outline-primary"
+                  htmlFor="appearance_auto"
+                >
+                  Automatisch
+                </label>
 
-                  <input type="radio" className="btn-check" name="appearance" value="light" id="appearance_light" onChange={this.applyAppearance} checked={this.props.settings.darkmode === false} />
-                  <label className="btn btn-outline-primary" htmlFor="appearance_light">Hell</label>
-                </div>
-                <div className="form-text">Bei "Automatisch" wird das Erscheinungsbild automatisch dem des Systems oder Browsers angepasst.</div>
+                <input
+                  type="radio"
+                  className="btn-check"
+                  name="appearance"
+                  value="light"
+                  id="appearance_light"
+                  onChange={this.applyAppearance}
+                  checked={this.props.settings.darkmode === false}
+                />
+                <label
+                  className="btn btn-outline-primary"
+                  htmlFor="appearance_light"
+                >
+                  Hell
+                </label>
+              </div>
+              <div className="form-text">
+                Bei "Automatisch" wird das Erscheinungsbild automatisch dem des
+                Systems oder Browsers angepasst.
+              </div>
             </div>
-
           </Dashboard.Tile>
         </Dashboard.Column>
         <Dashboard.Column sizes={{ lg: 4 }}>
-          <Dashboard.Tile title="Neuste Updates" help="Neue Funktionen, Bugfixes und Änderungen">
-            <div className="ms-1 mt-1">
-              <h6>
-                <b>6. Dezember 2022</b>
-              </h6>
-              <ul className="small">
-                <li>Beta-Status der To-do-Listen entfernt</li>
-              </ul>
-              <h6>
-                <b>21. Oktober 2022</b>
-              </h6>
-              <ul className="small">
-                <li>Visuelle Fehlerbehebungen</li>
-              </ul>
-              <h6>
-                <b>20. Oktober 2022</b>
-              </h6>
-              <ul className="small">
-                <li>Kalender und To-do-Listen aktualisiert</li>
-              </ul>
-              <h6>
-                <b>18. Oktober 2022</b>
-              </h6>
-              <ul className="small">
-                <li>Verbesserungen bei To-do-Listen</li>
-                <li>Diverse Fehlerbehebungen und Korrekturen</li>
-              </ul>
-              <h6>
-                <b>12. Oktober 2022</b>
-              </h6>
-              <ul className="small">
-                <li>Kalender-Kacheln umsortiert</li>
-                <li>Diverse Fehlerbehebungen & Verbesserungen</li>
-              </ul>
-              <h6>
-                <b>11. Oktober 2022</b>
-              </h6>
-              <ul className="small">
-                <li>Diverse Fehlerbehebungen</li>
-              </ul>
-              <h6>
-                <b>10. Oktober 2022</b>
-              </h6>
-              <ul className="small">
-                <li>Verbesserungen im Arbeitszeit-Dashboard</li>
-              </ul>
-              <h6>
-                <b>9. Oktober 2022</b>
-              </h6>
-              <ul className="small">
-                <li>Statistik für Arbeitszeit hinzugefügt</li>
-              </ul>
-              <h6>
-                <b>15. September 2022</b>
-              </h6>
-              <ul className="small">
-                <li>Einstellung für Erscheinungsbild hinzugefügt</li>
-              </ul>
-              <h6>
-                <b>11. September 2022</b>
-              </h6>
-              <ul className="small">
-                <li>To-do-Listen sind nun in der Beta</li>
-                <li>Diverse Fehlerbehebungen</li>
-              </ul>
-              <h6>
-                <b>10. September 2022</b>
-              </h6>
-              <ul className="small">
-                <li>Duplizieren-Knopf zu Kalenderereignissen hinzugefügt</li>
-                <li>Startseite &amp; In-App-Startseite hinzugefügt</li>
-              </ul>
-              <h6>
-                <b>5. September 2022</b>
-              </h6>
-              <ul className="small mb-1">
-                <li>Besseres Abonnier-Erlebnis bei Kalendern</li>
-                <li>Kalender-Features sind nun aus der Beta</li>
-              </ul>
+          <Dashboard.Tile
+            title="Neuste Updates"
+            help="Neue Funktionen, Bugfixes und Änderungen"
+          >
+            <div className="m-1">
+              {changelogitems.map((item, i) => [
+                <h6 key={i + "header"}>
+                  <b>{getDateString(new Date(item.date))}</b>
+                </h6>,
+                <ul key={i + "list"} className="small">
+                  {item.changes.map((change, j) => (
+                    <li key={j}>{change}</li>
+                  ))}
+                </ul>,
+              ])}
+              {this.state.expandChangelog ? (
+                <a
+                  className="btn btn-outline-primary btn-sm"
+                  onClick={() => this.setState({ expandChangelog: false })}
+                  style={{ marginTop: "-0.75rem" }}
+                >
+                  Weniger anzeigen
+                </a>
+              ) : (
+                <a
+                  className="btn btn-outline-primary btn-sm"
+                  onClick={() => this.setState({ expandChangelog: true })}
+                  style={{ marginTop: "-0.75rem" }}
+                >
+                  Mehr anzeigen
+                </a>
+              )}
             </div>
           </Dashboard.Tile>
         </Dashboard.Column>
@@ -145,3 +155,4 @@ export default class Page_Home extends React.Component {
     );
   }
 }
+
---
+++ teamized/app/src/data/changelog.js
@@ -0,0 +1,56 @@
+// Teamized changelog
+
+export const CHANGELOG = [
+    {
+        date: "2023-07-20",
+        changes: ["Änderungsprotokoll aktualisiert", "Diverse Fehlerbehebungen"]
+    },
+    { 
+        date: "2022-12-06",
+        changes: ["Beta-Status der To-do-Listen entfernt"]
+    },
+    {
+        date: "2022-10-21",
+        changes: ["Visuelle Fehlerbehebungen"]
+    },
+    {
+        date: "2022-10-20",
+        changes: ["Kalender und To-do-Listen aktualisiert"]
+    },
+    {
+        date: "2022-10-18",
+        changes: ["Verbesserungen bei To-do-Listen", "Diverse Fehlerbehebungen und Korrekturen"]
+    },
+    {
+        date: "2022-10-12",
+        changes: ["Kalender-Kacheln umsortiert", "Diverse Fehlerbehebungen & Verbesserungen"]
+    },
+    {
+        date: "2022-10-11",
+        changes: ["Diverse Fehlerbehebungen"]
+    },
+    {
+        date: "2022-10-10",
+        changes: ["Verbesserungen im Arbeitszeit-Dashboard"]
+    },
+    {
+        date: "2022-10-09",
+        changes: ["Statistik für Arbeitszeit hinzugefügt"]
+    },
+    {
+        date: "2022-09-15",
+        changes: ["Einstellung für Erscheinungsbild hinzugefügt"]
+    },
+    {
+        date: "2022-09-11",
+        changes: ["NEU: To-Do-Listen [BETA]", "Diverse Fehlerbehebungen"]
+    },
+    {
+        date: "2022-09-10",
+        changes: ["Duplizieren-Knopf zu Kalenderereignissen hinzugefügt", "Startseite & In-App-Startseite hinzugefügt"]
+    },
+    {
+        date: "2022-09-05",
+        changes: ["Besseres Abonnier-Erlebnis bei Kalendern", "Kalender-Features sind nun aus der Beta"]
+    }
+]
