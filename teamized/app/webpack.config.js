84cbeda543f22e499db107b8cc15056fb619e9cf
rafaelurben <github@rafaelurben.ch>
2024-10-30 13:37:33 +0100

    chore(ts): Setup typescript and prettier

--- teamized/app/webpack.config.js
+++ teamized/app/webpack.config.js
@@ -2,26 +2,34 @@ const path = require('path');
 
 module.exports = [
     {
-		name: "mainapp",
-		// mode: "development" || "production",
-        entry: "./src/app.js",
+        name: 'mainapp',
+        // mode: "development" || "production",
+        entry: './src/app.js',
         output: {
             path: path.resolve(__dirname, '../static/teamized/app/'),
-            filename: "mainapp.js"
+            filename: 'mainapp.js',
+        },
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js', '.tsx'],
         },
         module: {
             rules: [
                 {
-                    test: /\.(js|jsx)$/,
+                    test: /\.(js|ts)x?$/,
                     exclude: /node_modules/,
                     use: {
                         loader: 'babel-loader',
                         options: {
-                          presets: ["@babel/preset-react"]
-                        }
+                            presets: [
+                                '@babel/preset-react',
+                                '@babel/preset-typescript',
+                                '@babel/preset-env',
+                            ],
+                            plugins: ['@babel/plugin-transform-runtime'],
+                        },
                     },
-                }
-            ]
-        }
-	}
-]
+                },
+            ],
+        },
+    },
+];
